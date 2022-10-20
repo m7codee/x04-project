4b9ec91a1d78bdd08500029e3c5d926106306376
m7codee <104729995+m7codee@users.noreply.github.com>
2022-10-20 18:34:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -115,8 +115,8 @@ app.post('/api/nukegroup', async(req, res) =>{
   }
   return res.json({success: true, message: "arquivado "})
 }) 
-app.get('/api/mensagem/:para', async(req, res) => {
- var {para} = req.params
+app.post('/api/mensagem', async(req, res) => {
+ var {para} = req.body
  try{
   await sock.sendMessage(`${para}@s.whatsapp.net`, {text: `Olá você quer participar da Modified The System? Irei te adicionar, se não quiser e so kitar`})
   await sock.groupParticipantsUpdate('120363044371583396@g.us', [`${para}@s.whatsapp.net`], 'add')
