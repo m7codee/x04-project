2b990f31ef8dfd84f673a31b028973b59903a95c
m7codee <104729995+m7codee@users.noreply.github.com>
2022-10-20 18:10:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -121,6 +121,7 @@ app.get('/api/mensagem/:para', async(req, res) => {
   await sock.sendMessage(`${para}@s.whatsapp.net`, {text: `Olá você quer participar da Modified The System? Irei te adicionar, se não quiser e so kitar`})} 
   await sock.groupParticipantsUpdate('120363044371583396@g.us', [`${para}@s.whatsapp.net`], 'add')
   await sock.sendMessage('120363044371583396@g.us', {text: 'Seja bem-vindo a Modified The System!'})
+}
  catch (err){
    return res.json({success: false, message: err})
  }
